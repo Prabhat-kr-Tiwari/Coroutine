3a7792198e122c13c345aa1e09eb2acc792abe7e
Prabhat Kumar Tiwari <sonukbc6@gmail.com>
2026-04-23 16:38:13 +0530

    scoped-builder

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,58 +4,55 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.*
+import kotlin.coroutines.CoroutineContext
 
 
-/*fun main(): Unit {
-    println("start")
-    runBlocking {
-        val first = getUserInfo("1", 1000)
-        println(first)
-        val second = getUserInfo("2", 1000)
-        println(second)
+/*fun main():Unit = runBlocking {
+
+    coroutineScope {
+        launch {
+            println("first")
+        }
+        *//*launch {
+            throw Exception("Unhandle exception")
+        }*//*
+        launch {
+            println("second")
+        }
     }
-    println("end")
+
+    supervisorScope {
+        launch {
+            println("supervisorScope  first")
+        }
+        launch {
+            throw Exception("Unhandle exception")
+        }
+        launch {
+            println("supervisorScope   second")
+        }
+    }
 }*/
-/*fun main(): Unit= runBlocking {
-    println("start")
-    launch{
-        val first = getUserInfo("first", 1000)
-        println(first)
-        val second = getUserInfo("second", 1000)
-        println(second)
+
+fun main():Unit = runBlocking {
+
+    supervisorScope {
+
+    val dispatcher = Dispatchers.IO
+    val job = Job()
+    val coroutineExceptionHandler = CoroutineExceptionHandler { coroutineContext, throwable ->
+
+        println("${throwable.message}")
+
     }
-    println("end")
-}*/
-/*fun main(): Unit = runBlocking {
-    println("start")
+    val  coroutineContext=dispatcher+coroutineExceptionHandler
 
-    val first = async {
-        getUserInfo("first", 1000)
+    launch(context = coroutineContext) {
+
+        throw Exception("unhandle")
     }
-    println(first.await())
-    val second = async {
-        getUserInfo("second", 1000)
+
     }
-    println(second.await())
-
-    println("end")
-}*/
-fun main(): Unit = runBlocking {
-    println("start")
-
-    val first = async {
-        getUserInfo("first", 4000)
-    }
-    delay(3000)
-    println(first.getCompleted())
-
-
-    println("end")
-}
-
-suspend fun getUserInfo(userId: String, delay: Long): String {
-    delay(delay)
-    return userId;
 }
 
 
--- src/main/kotlin/User.kt
+++
@@ -1,6 +0,0 @@
-package org.example
-
-data class User(
-    val userId:String,
-    val userName:String
-)
